900c8f7963de3fedaf81a158b2393e13c1183362
AnkushKumar0408 <162012783+AnkushKumar0408@users.noreply.github.com>
2024-03-16 19:24:38 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+let arr = [
+    {dp:"https://plus.unsplash.com/premium_photo-1707413391413-057362d6eb3f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story:"https://images.unsplash.com/photo-1677286152129-5965d96c7005?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {dp:"https://images.unsplash.com/photo-1681661685356-5a8cf1710038?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story:"https://images.unsplash.com/photo-1683219151464-bfa270e6ba58?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {dp:"https://images.unsplash.com/photo-1708705177383-c899ae3859c9?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story:"https://plus.unsplash.com/premium_photo-1675597047092-e445f47d4b48?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {dp:"https://images.unsplash.com/photo-1708806016675-dac47ebc8459?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story:"https://images.unsplash.com/photo-1643919436901-e50c6f05dd2e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {dp:"https://plus.unsplash.com/premium_photo-1663951252355-e2f1d8d27d16?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story:"https://images.unsplash.com/photo-1709338430706-c0bee5469038?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {dp:"https://images.unsplash.com/photo-1591824438708-ce405f36ba3d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story:"https://images.unsplash.com/photo-1497206365907-f5e630693df0?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+]
+
+let storiya = document.querySelector("#storiya")
+let clutter = ""
+arr.forEach(function(elem,indx){
+    clutter += `<div class="story">
+    <img id="${indx}" src="${elem.dp}" alt="">
+</div>`
+})
+
+storiya.innerHTML = clutter;
+
+storiya.addEventListener("click", function(dets){
+    document.querySelector("#full-screen").style.display = "block"
+    document.querySelector("#full-screen").style.backgroundImage = `url(${arr[dets.target.id].story})`
+
+    setTimeout(function(){
+    document.querySelector("#full-screen").style.display = "none"
+    },3000)
+});
+
+var name = "Ankush";
+var roll_no = 30;
+console.log(name+roll_no);
